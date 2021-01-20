e60ec1fe400efd457f4a913ba60234d266d69432
ravi yadav <raviyadav00063@gmail.com>
2021-01-20 22:58:40 +0530

    Working on home page

--- src/Home.js
+++ src/Home.js
@@ -14,17 +14,17 @@ function Home() {
             <div className="home_row">
                 <Product />
                 <Product />
-                {/* Product */}
+               
             </div>
 
             <div className="home_row">
-                {/* Product */}
-                {/* Product */}
-                {/* Product */}
+                <Product />
+              <Product />
+                <Product />
             </div>
 
             <div className="home_row">
-                {/* Product */}
+                <Product />
             </div>
         </div>
     </div>
