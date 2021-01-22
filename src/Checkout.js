88cf287a25f98a37c03dd47dbd03c4343807565d
ravi yadav <raviyadav00063@gmail.com>
2021-01-22 21:10:57 +0530

    working on checkout page

--- src/Checkout.js
+++ src/Checkout.js
@@ -17,7 +17,7 @@ function Checkout() {
            </div>
            <div className="checkout_right">
                <Subtotal/>
-               <h2>The subtotal will go here</h2>
+              
            </div>
         </div>
     )
