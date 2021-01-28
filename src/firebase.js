7c9d83a82f736c3eb643914f6930c529946941cc
ravi yadav <raviyadav00063@gmail.com>
2021-01-28 22:50:18 +0530

    The Authentication is being implemented

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,6 @@
+import firebase from "firebase";
+
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyA-5m2KjymOBSpIdFkZlvqh99ndUW-h6Ok",
@@ -8,3 +11,10 @@ const firebaseConfig = {
     appId: "1:484295578901:web:94b68e535d2914bc1e451c",
     measurementId: "G-3MDVED1TNX"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+
+  export { db, auth };
